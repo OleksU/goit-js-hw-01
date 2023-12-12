8d8e9b2dcf0c56e52400265f096c21217cb53350
OleksU <cibulskiy089@gmail.com>
2023-12-12 22:19:39 +0100

    Update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border){
-    return content + (padding * 2) + (border * 2);
+    return (parseInt(content) + (parseInt(padding) * 2) + (parseFloat(border) * 2));
 };
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
